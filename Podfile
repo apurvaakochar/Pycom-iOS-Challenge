17bd121e3bf8ca66a06cf053949447470e8efb43
Apurva Kochar <apurvakochar@apurvas-macbook-pro.local>
2018-04-30 21:26:07 +0200

    Removed  AFNetworking

--- Podfile
+++ Podfile
@@ -2,6 +2,5 @@
 target 'JodelChallenge' do
     use_frameworks!
   pod 'FlickrKit'
-  pod 'AFNetworking'
   pod 'SDWebImage', '~> 4.0'
 end
--- Pods/Target Support Files/Pods-JodelChallenge/Pods-JodelChallenge-frameworks.sh
+++ Pods/Target Support Files/Pods-JodelChallenge/Pods-JodelChallenge-frameworks.sh
@@ -134,12 +134,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FlickrKit/FlickrKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FlickrKit/FlickrKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
