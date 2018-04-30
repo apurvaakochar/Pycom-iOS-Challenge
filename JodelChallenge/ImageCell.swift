ae090ccc9635863df4e1c41e704bf8dee5701cf3
Apurva Kochar <apurvakochar@apurvas-macbook-pro.local>
2018-04-30 21:15:27 +0200

    missing dependency

---
+++ JodelChallenge/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  FullScreenCell.swift
+//  JodelChallenge
+//
+//  Created by Apurva Kochar on 4/29/18.
+//  Copyright © 2018 Jodel. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet weak var fullImageView: UIImageView!
+}
